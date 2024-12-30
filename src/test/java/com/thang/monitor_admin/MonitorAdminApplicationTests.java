2a13d9eac2473d0912a8b882a4c849b31b948520
thangnguyen1992 <ntthang4@cmcglobal.vn>
2024-12-30 16:15:26 +0700

    done

---
+++ src/test/java/com/thang/monitor_admin/MonitorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thang.monitor_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
